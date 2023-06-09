02831ff9161ff4d5fd44884d32f31a0c21e7de0a
hotine <hotine@163.com>
2023-06-09 14:56:13 +0800

    Update 4ksj.py

--- 4ksj.py
+++ 4ksj.py
@@ -3,17 +3,14 @@ import httpx
 import re
 import sys
 import time
-#import logging
-#from logging.handlers import RotatingFileHandler
 from urllib.parse import urlencode
 
 # global variables
-uname = 'XXXXXXXXXXXXXX'         # username
-upassword = 'XXXXXXXXXXXXXXX'     # password
+uname = '**********'         # username，需要修改
+upassword = '**********'     # password，需要修改
 loginhash = ''
-logpath = '/home/anaconda/4ksj.log'
+formhash = ''
 r = httpx.Client(http2=True)
-#r = httpx.Client(http2=True, proxsies='http://127.0.0.1:8080', verify=False)
 headers = {'Host':'www.4ksj.com',
 'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0',
 'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
@@ -27,24 +24,23 @@ headers = {'Host':'www.4ksj.com',
 'Te':'trailers'
 }
 
+#从个人空间页面获取当前K值
 def getK(spaceurl):
-    #logger = logging.getLogger('4ksj')
     ret = r.get(spaceurl, headers = headers).text
     time.sleep(1)
     K_now = re.findall(r'<li><em>K币</em>(.*?) 个</li>', ret)[0]
-    #logger.info('当前K币: ' + str(K_now) + '个')
-    print('当前K币: ' + str(K_now) + '个')
+    #print('当前K币: ' + str(K_now) + '个')
     return K_now
 
-def login(uname, upassword, formhash):
-    #logger = logging.getLogger('4ksj')
+#登录网站，并获取个人空间入口
+def login(uname, upassword):
     headers['Referer'] = 'https://www.4ksj.com/'
     ret = r.get(r'https://www.4ksj.com/member.php?mod=logging&action=login', headers=headers).text
     time.sleep(1)
-    #print("登录页内容"+ret)
+    formhash = re.findall(r'<input type="hidden" name="formhash" value="(.*?)"', ret)[0]
+    print('获取到登录页formhash: ' + formhash)
     loginhash = re.findall(r'loginhash=(.*?)"', ret)[0]
-    #logger.debug('loginhash' + loginhash)
-    print("获取到loginhash:"+loginhash)
+    print("获取到登录页loginhash:"+loginhash)
     headers['Origin'] = 'https://www.4ksj.com'
     headers['Referer'] = 'https://www.4ksj.com/member.php?mod=logging&action=login'
     headers['Sec-Fetch-Dest'] = 'iframe'
@@ -53,10 +49,8 @@ def login(uname, upassword, formhash):
     ret = r.post('https://www.4ksj.com/member.php?mod=logging&action=login&loginsubmit=yes&loginhash='+loginhash+'&inajax=1', headers = headers, data=urlencode(data)).text
     time.sleep(1)
     if 'succeedmessage' in ret:
-        #logger.info('登录成功')
         print('登录成功')
     else:
-        #logger.error(re.findall(r'errorhandle_\(\'(.*?)\', {', ret)[0])
         print(re.findall(r'errorhandle_\(\'(.*?)\', {', ret)[0])
         return False, False
 
@@ -64,21 +58,20 @@ def login(uname, upassword, formhash):
     del headers['Content-Type']
     headers['Sec-Fetch-Dest'] = 'document'
     ret = r.get('https://www.4ksj.com', headers=headers).text
-    print("--------------------------------------------------------------------------------------")
-    #print(ret)
+
     spaceurl = ""
     userid = re.findall(r'discuz_uid = \'(.*?)\'', ret)[0]
     spaceurl = "https://www.4ksj.com/space-uid-"+userid+".html"
     print("个人空间入口:"+spaceurl)
     return spaceurl
 
+#进行签到
 def qiandao():
-    #logger = logging.getLogger('4ksj')
     headers['Referer'] = spaceurl
     ret = r.get('https://www.4ksj.com/qiandao/', headers = headers).text
     time.sleep(1)
     formhash = re.findall(r'action=logout&amp;formhash=(.*?)"', ret)[0]
-    print('formhash2: ' + formhash)
+    print('获取到签到页formhash: ' + formhash)
 
     headers['Referer'] = 'https://www.4ksj.com/qiandao/'
     headers['Sec-Fetch-Dest'] = 'empty'
@@ -92,12 +85,12 @@ def qiandao():
     ret = r.get('https://www.4ksj.com//qiandao/?mod=sign&operation=qiandao&formhash=' + formhash + '&format=empty&inajax=1&ajaxtarget=', headers = headers).text
     time.sleep(1)
     if '今日已签' in ret:
-        print('今日已签')
+        print('签到结果：今日已签')
     if '<root><![CDATA[]]></root>' in ret:
-        print('签到成功')
+        print('签到结果：签到成功')
 
+#退出登录
 def logout(formhash):
-    #logger = logging.getLogger('4ksj')
     ret = r.get('https://www.4ksj.com/member.php?mod=logging&action=logout&formhash=' + formhash, headers = headers).text
     print('退出登录')
 
@@ -109,17 +102,13 @@ if __name__ == '__main__':
             #logger.warn('retrying for ' + str(i) + 'times')
             print('尝试第' + str(i) + '次')
         try:
-            ret = r.get('https://www.4ksj.com/member.php?mod=logging&action=login', headers=headers).text
-            time.sleep(1)
-            #print(ret)
-            formhash = re.findall(r'<input type="hidden" name="formhash" value="(.*?)"', ret)[0]
-            #logger.debug('formhash1: ' + formhash)
-            print('获取到formhash1: ' + formhash)
-            spaceurl = login(uname, upassword, formhash)
-            if not spaceurl:
-                print("没有获取到昵称，签到失败，系统终止。")
-                sys.exit(0) # exit if login failed
+            spaceurl = login(uname, upassword)
+            #if not spaceurl:
+            #    print("没有获取到昵称，签到失败，系统终止。")
+            #    sys.exit(0) # exit if login failed
             headers['Referer'] = 'https://www.4ksj.com/'
+
+            k_num1 = getK(spaceurl)
 
             qiandao()
 
@@ -130,11 +119,17 @@ if __name__ == '__main__':
             headers['Upgrade-Insecure-Requests'] = '1'
             del headers['X-Requested-With']
             
-            getK(spaceurl)
+            k_num2 = getK(spaceurl)
+
+            print('***************结果统计***************')
+            #print('之前K币: ' + str(k_num1) + '个')
+            print('本次获得K币: ' + str(int(k_num2)-int(k_num1)) + '个')
+            print('累计K币: ' + str(int(k_num2)) + '个')
+            print('*************************************')
 
             logout(formhash)
+
             sys.exit(0)
         except Exception as e:
-            #logger.critical('line: ' + str(e.__traceback__.tb_lineno) + ' ' + repr(e))
             print('line: ' + str(e.__traceback__.tb_lineno) + ' ' + repr(e))
             time.sleep(10)
